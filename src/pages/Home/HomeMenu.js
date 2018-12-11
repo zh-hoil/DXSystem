9901e3e2388dc22c65a472cb3554943514e63800
gaokung <gaokung@yonyou.com>
2018-12-11 22:10:50 +0800

    *修改首页样式

--- src/pages/Home/HomeMenu.js
+++ src/pages/Home/HomeMenu.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import MenuList from "Components/MenuList";
-import {getLocalStorage} from './../../utils';
+import { getLocalStorage } from "./../../utils";
 
 const menuList = [
-    { title: "公告", path: "/", icon: "" },
-    { title: "了解 NC Cloud", path: "/ncc", icon: "" },
+    { title: "公告", path: "/", icon: "icon-gonggao" },
+    { title: "了解 NC Cloud", path: "/ncc", icon: "icon-nccloud" },
     {
         title: "了解 NC",
-        path: `/NCapp3/index.html#Z-home?token=${getLocalStorage("YY_userInfo")}`,
-        icon: "",
+        path: `/NCapp3/index.html#Z-home?token=${getLocalStorage(
+            "YY_userInfo"
+        )}`,
+        icon: "icon-nc",
         iframe: true
     },
-    { title: "我的关注", path: "/", icon: "" },
-    { title: "关于 NC 一线牵", path: "/", icon: "" }
+    { title: "我的关注", path: "/", icon: "icon-guanzhu" },
+    { title: "关于 NC 一线牵", path: "/", icon: "icon-guanyunc" }
 ];
 
 class HomeMenu extends React.Component {
@@ -25,24 +27,19 @@ class HomeMenu extends React.Component {
         if (iframe) {
             return (
                 <a href={path} key={index} className="menu-item">
-                    <div
-                        style={{
-                            background: `url(${icon}) no-repeat center center`,
-                            backgroundSize: "contain"
-                        }}
-                    />
+                    <div className="menu-icon">
+                        <i className={`iconfont ${icon}`} />
+                    </div>
+
                     <span>{title}</span>
                 </a>
             );
         }
         return (
             <Link key={index} to={path} className="menu-item">
-                <div
-                    style={{
-                        background: `url(${icon}) no-repeat center center`,
-                        backgroundSize: "contain"
-                    }}
-                />
+                <div className="menu-icon">
+                    <i className={`iconfont ${icon}`} />
+                </div>
                 <span>{title}</span>
             </Link>
         );
