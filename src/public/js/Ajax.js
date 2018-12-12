35518c7978e8f1014dcd6a6deedaa24759045c2d
gaokung <gaokung@yonyou.com>
2018-12-12 17:25:22 +0800

    *修改userid获取

--- src/public/js/Ajax.js
+++ src/public/js/Ajax.js
@@ -1,13 +1,20 @@
 import axios from "axios";
+import { getLocalStorage } from "./../../utils";
 
 const Ajax = axios.create({
-    transformRequest: [function (data) {
-        let ret = ''
-        for (let it in data) {
-            ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+    transformRequest: [
+        function(data) {
+            let ret = "";
+            for (let it in data) {
+                ret +=
+                    encodeURIComponent(it) +
+                    "=" +
+                    encodeURIComponent(data[it]) +
+                    "&";
+            }
+            return ret.substring(0, ret.length - 1);
         }
-        return ret.substring(0, ret.length-1)
-    }],
+    ]
 });
 
 // '/commentTopic'
@@ -19,34 +26,37 @@ const Ajax = axios.create({
 //     modules: "eg"
 // }
 
-export const userId = "0001AA1000000002W4SU";
+export const userId =
+    getLocalStorage("YY_userInfo")
+        ? getLocalStorage("YY_userInfo")
+        : "0001AA1000000002W4SU";
 
 export function Post(url, data, resolve, reject) {
     Ajax.post(url, data)
-    .then(function (response) {
-        if(response.status === 200) {
-            resolve && resolve(response.data)
-        }else{
-            reject && reject(response.statusText)
-        }
-    })
-    .catch(function (error) {
-        reject && reject(error)
-    });
+        .then(function(response) {
+            if (response.status === 200) {
+                resolve && resolve(response.data);
+            } else {
+                reject && reject(response.statusText);
+            }
+        })
+        .catch(function(error) {
+            reject && reject(error);
+        });
 }
 
 export function Get(url, data, resolve, reject) {
     Ajax.get(url, {
         params: data
     })
-    .then(function (response) {
-        if(response.status === 200) {
-            resolve && resolve(response.data)
-        }else{
-            reject && reject(response.statusText)
-        }
-    })
-    .catch(function (error) {
-        reject && reject(error)
-    });
+        .then(function(response) {
+            if (response.status === 200) {
+                resolve && resolve(response.data);
+            } else {
+                reject && reject(response.statusText);
+            }
+        })
+        .catch(function(error) {
+            reject && reject(error);
+        });
 }
