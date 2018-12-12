3bdc9d823e713875e4674301ff94a739f93a5834
gaokung <gaokung@yonyou.com>
2018-12-12 12:55:19 +0800

    +新增简单菜单列表

---
+++ src/components/SimpleListMenu/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.less";
+const SimpleListMenu = props => {
+    return (
+        <div className={`simple_list_menu ${props.className}`}>
+            {props.menuList.map((item, index) => {
+                return (
+                    <a key={index} href={`${item.path}`} className="list_item">
+                        <div className="list_item_left">
+                            <div className="list_item_icon">
+                                <i className={`iconfont ${item.icon}`} />
+                            </div>
+                            <span>{item.title}</span>
+                        </div>
+                        <i className="iconfont icon-more" />
+                    </a>
+                );
+            })}
+        </div>
+    );
+};
+export default SimpleListMenu;
