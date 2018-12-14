3bd716bf18c0c99bca34444e0f1854760f51e729
zhanghuanp <zhanghuanp@yonyou.com>
2018-12-14 16:52:02 +0800

    *修复数据无法展示bug

--- src/pages/ThemeSearch/index.js
+++ src/pages/ThemeSearch/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Drawer, Tabs, Radio, Checkbox, List } from "antd-mobile";
-import { updateData } from "Store/ThemeSearch/action";
 import SearchBar from "Components/SearchBar";
-import { userId, Get } from "Public/js/Ajax";
-import { hasItemInArr } from "Src/utils";
 import ThemeList from "./ThemeList";
+import { Drawer, Tabs, Radio, Checkbox, List } from 'antd-mobile';
 import "./index.less";
+import { userId, Get, Post } from "Public/js/Ajax";
+import { connect } from "react-redux";
+import { updateData } from "Store/ThemeSearch/action";
+import { hasItemInArr } from "Src/utils";
 
-const THEMEFIELDURL = "/getNCCloudThemeField";
 const THEMEFIELDVERDSIONURL = "/getNCCloudThemeVersion";
 
 const RadioItem = Radio.RadioItem;
@@ -19,7 +18,7 @@ const tabs = [
     { title: "版本" },
     { title: "主题类型" },
     { title: "状态" }
-];
+]
 let filterVersion = [];
 let filterType = [];
 let filterStatus = [];
@@ -28,82 +27,49 @@ class ThemeSearch extends React.Component {
         super(props);
         this.state = {
             searchBoolean: false,
-            themeFieldId: "",
+            // themeFieldId: "",
             filter: "",
             page: 0,
             version: [],
             type: [
-                { type: "功能特性", checked: true },
-                { type: "应用方案", checked: true },
-                { type: "最佳实践", checked: true },
-                { type: "竞争分析", checked: true },
-                { type: "培训课件", checked: true },
-                { type: "移动app", checked: true },
-                { type: "微信应用", checked: true }
+                { type: "功能特性" },
+                { type: "应用方案" },
+                { type: "最佳实践" },
+                { type: "竞争分析" },
+                { type: "培训课件" },
+                { type: "移动app" },
+                { type: "微信应用" },
             ],
             status: [
-                { status: "正式版", checked: true },
-                { status: "孵化版", checked: true },
-                { status: "在研", checked: true },
-                { status: "预研", checked: true }
+                { status: "正式版" },
+                { status: "傅桦斑" },
+                { status: "在研" },
+                { status: "预研" }
             ]
-        };
+        }
     }
 
     componentWillMount() {
-        this._getThemeFields();
-        this._getThemeVersion();
+        this._getThemeVersion()
     }
 
-    //获取主题域数据
-    _getThemeFields() {
-        Get(
-            THEMEFIELDURL,
-            {
-                userId: userId
-            },
-            res => {
-                let themeFields = res.data;
-                let themeFieldId = themeFields[0].id;
-                this.setState({
-                    themeFieldId
-                });
-                if (this.props.updateData) {
-                    this.props.updateData({ themeFieldId, themeFields });
-                }
-            },
-            err => {
-                console.log(err);
-            }
-        );
-    }
+    
 
-    //获取版本数据
     _getThemeVersion() {
-        Get(
-            THEMEFIELDVERDSIONURL,
-            {
-                userId: userId
-            },
-            res => {
-                let version = res.data;
-                version.map((item)=>item.checked = true);
-                console.log("--------------",version);
-                this.setState({
-                    version: version
-                });
-            }
-        );
+        Get(THEMEFIELDVERDSIONURL, {
+            userId: userId
+        }, (res) => {
+            let version = res.data;
+            this.setState({
+                version: version
+            })
+        }, (err) => {
+            console.log(err)
+        })
     }
 
-    //重置数据
     _backState(page) {
         switch (page) {
-            case 1:
-                this.setState({
-                    themeFieldId: this.props.themeFieldId
-                });
-                break;
             case 2:
                 filterVersion = [];
                 break;
@@ -116,229 +82,188 @@ class ThemeSearch extends React.Component {
         }
     }
 
-    //取消按钮事件
     onOpenChange() {
         if (this.props.open) {
             if (this.state.page > 0) {
                 //恢复默认数据
-                this._backState(this.state.page);
+                this._backState(this.state.page)
                 this.setState({
                     page: 0
-                });
+                })
             } else {
                 //关闭弹窗
                 if (this.props.updateData) {
-                    this.props.updateData({ open: !this.props.open });
+                    this.props.updateData({ open: !this.props.open })
                 }
             }
         }
+
     }
 
-    //获取某数组字符串
-    _getArrString(arr) {
+    _getArrString (arr) {
         if (arr.length) {
             let str = "";
             for (let item of arr) {
-                str = str + item + ",";
+                str = str + item + ","
             }
-            return str.substring(0, str.length - 1);
+            return ("version=" + str.substring(0, str.length - 1));
         }
     }
 
-    //获取筛选字符串
     _getFilterKeys() {
         let keys = [];
-
-        filterVersion = this.state.version.filter((item)=>item.checked).map((item)=>item.version);
-        filterType = this.state.type.filter((item)=>item.checked).map((item)=>item.type);
-        filterStatus = this.state.status.filter((item)=>item.checked).map((item)=>item.status);
-
-        if (this._getArrString(filterVersion)) {
+        if(this._getArrString(filterVersion)){
             keys.push(this._getArrString(filterVersion));
         }
-        if (this._getArrString(filterType)) {
+        if(this._getArrString(filterType)){
             keys.push(this._getArrString(filterType));
         }
-        if (this._getArrString(filterStatus)) {
+        if(this._getArrString(filterStatus)){
             keys.push(this._getArrString(filterStatus));
         }
-        if (keys.length) {
-            return keys.join("&");
-        } else {
-            return "";
+        if(keys.length) {
+            return keys.join("&")
+        }else{
+            return ""
         }
+        
     }
 
-    //确定按钮
     handleFilter() {
         if (this.props.open) {
             if (this.state.page > 0) {
                 this.setState({
                     page: 0
                     //再设置其他数据
-                });
+                })
             } else {
                 //关闭弹窗
                 if (this.props.updateData) {
-                    this.props.updateData({
-                        open: !this.props.open,
-                        themeFieldId: this.state.themeFieldId
-                    });
+                    this.props.updateData({ open: !this.props.open, themeFieldId: this.state.themeFieldId })
+
                 }
                 let filter = this._getFilterKeys();
-                console.log(filter);
+                console.log(filter)
                 this.setState({
                     filter
-                });
+                })
             }
         }
     }
 
-    //主题域选择
     onFieldChange(themeFieldId) {
         this.setState({
             themeFieldId
-        });
+        })
     }
 
-    //切换页面
+
+    onVersionChange(version) {
+        if (hasItemInArr(filterVersion, version)) {
+            let index = filterVersion.indexOf(version)
+            filterVersion.splice(index, 1)
+        } else {
+            filterVersion.push(version)
+        }
+    }
+
+    onTypeChange(type) {
+        if (hasItemInArr(filterType, type)) {
+            let index = filterType.indexOf(type)
+            filterType.splice(index, 1)
+        } else {
+            filterType.push(type)
+        }
+    }
+
+    onStatusChange(status) {
+        if (hasItemInArr(filterStatus, status)) {
+            let index = filterStatus.indexOf(status)
+            filterStatus.splice(index, 1)
+        } else {
+            filterStatus.push(status)
+        }
+    }
+
     handlePage(page) {
         this.setState({
             page: page
-        });
+        })
     }
 
     //点击搜索框执行的操作
     handleSearch() {
         const searchHistory = "/searchHistory";
         if (this.state.searchBoolean) {
-            return;
+            return
         }
-        window.location.hash = searchHistory;
+        window.location.hash = searchHistory
     }
 
     //筛选按钮执行的函数
     handleSearchBack() {
         if (this.props.updateData) {
-            this.props.updateData({ open: true });
+            this.props.updateData({ open: true })
         }
-    }
-    /**
-     *
-     * @param {*} type 数据源
-     * @param {*} index 下标
-     */
-    multipleControl(key, index) {
-        let arrList = this.state[key];
-        let item = JSON.parse(JSON.stringify(arrList[index]));
-        item.checked = !item.checked;
-        arrList[index] = item;
-        let obj = {};
-        obj[key] = arrList;
-        this.setState({
-            ...obj
-        });
     }
 
     render() {
         const sidebar = (
             <div className="sidebar-wrapper">
                 <div className="drawer-top">
-                    <span
-                        className="drawer-top-operation"
-                        onClick={this.onOpenChange.bind(this)}
-                    >
-                        返回
-                    </span>
+                    <span className="drawer-top-operation" onClick={this.onOpenChange.bind(this)}>返回</span>
                     <span className="drawer-top-title">筛选</span>
-                    <span
-                        className="drawer-top-operation"
-                        onClick={this.handleFilter.bind(this)}
-                    >
-                        确定
-                    </span>
+                    <span className="drawer-top-operation" onClick={this.handleFilter.bind(this)}>确定</span>
                 </div>
-                <Tabs
-                    tabs={tabs}
+                <Tabs tabs={tabs}
                     page={this.state.page}
-                    style={{ height: "100%", backgroundColor: "#fff" }}
+                    style={{ height: '100%', backgroundColor: '#fff' }}
                 >
-                    <div style={{ height: "100%", backgroundColor: "#fff" }}>
-                        <List >
-                            {tabs.map((item, i) => {
-                                if (i === 0) return;
-                                return (
-                                    <List.Item
-                                        key={i}
-                                        className='tabs_list_item'
-                                        onClick={this.handlePage.bind(this, i)}
-                                    >
-                                        <span>{item.title}</span>
-                                        <i className='iconfont icon-enter' />
-                                    </List.Item>
-                                );
-                            })}
+                    <div style={{ height: '100%', backgroundColor: '#fff' }}>
+                        <List>
+                            {
+                                tabs.map((item, i) => {
+                                    if (i === 0) return
+                                    return (
+                                        <List.Item key={i} onClick={this.handlePage.bind(this, i)}>{item.title} <span style={{ float: "right" }}>{">"}</span></List.Item>
+                                    )
+                                })
+                            }
                         </List>
                     </div>
-                    <div style={{ height: "100%", backgroundColor: "#fff" }}>
-                        <List  className='filtrate_list'>
+                    <div style={{ height: '100%', backgroundColor: '#fff' }}>
+                        <List>
                             {this.props.themeFields.map(i => (
-                                <RadioItem
-                                    key={i.id}
-                                    className="filtrate_list_item"
-                                    checked={this.state.themeFieldId === i.id}
-                                    onClick={() => this.onFieldChange(i.id)}
-                                >
+                                <RadioItem key={i.id} checked={this.state.themeFieldId === i.id} onClick={() => this.onFieldChange(i.id)}>
                                     {i.field}
                                 </RadioItem>
                             ))}
                         </List>
                     </div>
-                    <div style={{ height: "100%", backgroundColor: "#fff" }}>
-                        <List  className='filtrate_list'>
+                    <div style={{ height: '100%', backgroundColor: '#fff' }}>
+                        <List>
                             {this.state.version.map((i, index) => (
-                                <RadioItem
-                                    key={index}
-                                    checked={i.checked}
-                                    className="filtrate_list_item"
-                                    onClick={() => {
-                                        this.multipleControl("version", index);
-                                    }}
-                                >
+                                <CheckboxItem key={index} onChange={() => this.onVersionChange(i.version, index)}>
                                     {i.version}
-                                </RadioItem>
+                                </CheckboxItem>
                             ))}
                         </List>
                     </div>
-                    <div style={{ height: "100%", backgroundColor: "#fff" }}>
-                        <List  className='filtrate_list'>
+                    <div style={{ height: '100%', backgroundColor: '#fff' }}>
+                        <List>
                             {this.state.type.map((i, index) => (
-                                <RadioItem
-                                    key={index}
-                                    checked={i.checked}
-                                    className="filtrate_list_item"
-                                    onClick={() => {
-                                        this.multipleControl("type", index);
-                                    }}
-                                >
+                                <CheckboxItem key={index} onChange={() => { this.onTypeChange(i.type, index) }}>
                                     {i.type}
-                                </RadioItem>
+                                </CheckboxItem>
                             ))}
                         </List>
                     </div>
-                    <div style={{ height: "100%", backgroundColor: "#fff" }}>
-                        <List  className='filtrate_list'>
+                    <div style={{ height: '100%', backgroundColor: '#fff' }}>
+                        <List>
                             {this.state.status.map((i, index) => (
-                                <RadioItem
-                                    key={index}
-                                    checked={i.checked}
-                                    className="filtrate_list_item"
-                                    onClick={() => {
-                                        this.multipleControl("status", index);
-                                    }}
-                                >
+                                <CheckboxItem key={index} onChange={() => this.onStatusChange(i.status, index)}>
                                     {i.status}
-                                </RadioItem>
+                                </CheckboxItem>
                             ))}
                         </List>
                     </div>
@@ -354,27 +279,21 @@ class ThemeSearch extends React.Component {
                     open={this.props.open}
                     onOpenChange={this.onOpenChange.bind(this)}
                 >
-                    <SearchBar
-                        searchBoolean={this.state.searchBoolean}
+                    <SearchBar searchBoolean={this.state.searchBoolean}
                         handleSearch={this.handleSearch.bind(this)}
                         handleSearchBack={this.handleSearchBack.bind(this)}
                     />
-                    <ThemeList
-                        themeFields={this.props.themeFields}
-                        themeFieldId={this.props.themeFieldId}
-                        filter={this.state.filter}
-                    />
+                    <ThemeList filter={this.state.filter} />
                 </Drawer>
             </div>
-        );
+        )
     }
 }
-ThemeSearch = connect(
-    state => ({
-        open: state.themeSearchData.open,
-        themeFieldId: state.themeSearchData.themeFieldId,
-        themeFields: state.themeSearchData.themeFields
-    }),
+ThemeSearch = connect((state) => ({
+    open: state.themeSearchData.open,
+    themeFieldId: state.themeSearchData.themeFieldId,
+    themeFields: state.themeSearchData.themeFields,
+}),
     { updateData }
-)(ThemeSearch);
+)(ThemeSearch)
 export default ThemeSearch;
--- src/pages/ThemeSearch/themeList.js
+++ src/pages/ThemeSearch/themeList.js
@@ -3,65 +3,92 @@ import React from "react";
 import ThemeDocs from "Components/ThemeDocs";
 import { getTargetAttr } from "Src/utils";
 import { userId, Get, Post } from "Public/js/Ajax";
+import { connect } from "react-redux";
+import { updateData } from "Store/ThemeSearch/action";
 
 const THEMELISTURL = "/getNCCloudThemeList";
+const THEMEFIELDURL = "/getNCCloudThemeField";
 
 class ThemeList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            ...props,
             themeList: [],
             label: 1 //1 按好评排序 0 按时间排序
         };
     }
 
     componentWillMount() {
-        this.setState({
-            ...this.props
-        });
+        // this.setState({
+        //     ...this.props
+        // });
+        this._getThemeFields()
     }
 
     componentWillReceiveProps(nextProps) {
-        console.log(nextProps);
-        let themeFields = nextProps.themeFields;
-        let themeFieldId = nextProps.themeFieldId;
-        let filter = nextProps.filter;
-        this._getThemeList(themeFieldId, themeFields, this.state.label, filter);
+        // console.log(nextProps);
+        // let themeFields = nextProps.themeFields;
+        // let themeFieldId = nextProps.themeFieldId;
+        // let filter = nextProps.filter;
+        // this._getThemeList(themeFieldId, themeFields, this.state.label, filter);
+        // this._initData()
+        // this._getThemeList(this.props.themeFieldId, this.props.themeFields, this.state.label, this.props.filter);
+    }
+
+    // _initData () {
+    //     this._getThemeFields()
+    // }
+
+    _getThemeFields() {
+        Get(THEMEFIELDURL, {
+            userId: userId
+        }, (res) => {
+            let themeFields = res.data;
+            let themeFieldId = themeFields[0].id;
+
+
+            if (this.props.updateData) {
+                this.props.updateData({ themeFieldId, themeFields })
+            }
+
+            this._getThemeList(this.props.themeFieldId, this.props.themeFields, this.state.label, this.props.filter);
+            
+        }, (err) => {
+            console.log(err)
+        })
     }
 
     //获取主题域详细文档数据
     _getThemeList(themeFieldId, themeFields, label, filter) {
-        let filterArr = filter.split("&");
-        let version = filterArr[0];
-        let type = filterArr[1];
-        let status = filterArr[2];
+        let data = {
+            userId: userId,
+            fieldId: themeFieldId,
+            label: label
+        }
+        let version = "";
+        let type = "";
+        let status = ""
+        if(filter) {
+            let filterArr = filter.split("&");
+            let version = filterArr[0];
+            let type = filterArr[1];
+            let status = filterArr[2];
+            data = {
+                ...data,
+                version,
+                type, 
+                status
+            }
+        }
         Get(
             THEMELISTURL,
-            {
-                userId: userId,
-                fieldId: themeFieldId,
-                label: label,
-                version: version,
-                type: type,
-                status: status
-            },
+            data,
             res => {
                 let themeList = res.data;
-                if (themeFields) {
-                    this.setState({
-                        themeFields: themeFields,
-                        themeFieldId: themeFieldId,
-                        themeList: themeList,
-                        filter: filter
-                    });
-                } else {
-                    this.setState({
-                        themeFieldId: themeFieldId,
-                        themeList: themeList,
-                        filter: filter
-                    });
-                }
+                this.setState({
+                    themeList: themeList,
+                    filter: filter
+                })
             }
         );
     }
@@ -75,19 +102,22 @@ class ThemeList extends React.Component {
             //无变化
             return;
         }
+        if(this.props.updateData) {
+            this.props.updateData({themeFieldId: currentFieldId})
+        }
         this._getThemeList(
-            currentFieldId,
-            this.state.themeFields,
+            this.props.themeFieldId,
+            this.props.themeFields,
             this.state.label,
-            this.state.filter
+            this.props.filter
         );
     }
 
     handleSort(label) {
-        if (label === undefined) {
+        if (label == undefined) {
             return;
         }
-        if (label === this.state.label) {
+        if (label == this.state.label) {
             return;
         }
         // this._getThemeList(this.state.themeFieldId)
@@ -96,10 +126,10 @@ class ThemeList extends React.Component {
         });
 
         this._getThemeList(
-            this.state.themeFieldId,
-            this.state.themeFields,
+            this.props.themeFieldId,
+            this.props.themeFields,
             label,
-            this.state.filter
+            this.props.filter
         );
     }
 
@@ -115,12 +145,12 @@ class ThemeList extends React.Component {
             <div className="themes-wrapper">
                 <div className="catelog">
                     <ul onClick={this.handleSelectField.bind(this)}>
-                        {this.state.themeFields.map((field, index) => (
+                        {this.props.themeFields.map((field, index) => (
                             <li
                                 key={index}
                                 fieldid={field.id}
                                 className={
-                                    field.id == this.state.themeFieldId
+                                    field.id == this.props.themeFieldId
                                         ? "active"
                                         : ""
                                 }
@@ -158,4 +188,11 @@ class ThemeList extends React.Component {
         );
     }
 }
+ThemeList = connect((state) => ({
+    open: state.themeSearchData.open,
+    themeFieldId: state.themeSearchData.themeFieldId,
+    themeFields: state.themeSearchData.themeFields,
+}),
+    { updateData }
+)(ThemeList)
 export default ThemeList;
